5030db7d7b470bd39ffdd87c0cb3bc3a6ff59e73
S4977690 <91629502+S4977690@users.noreply.github.com>
2021-12-01 21:24:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     char a=a;
     cin >> a;
-    if(funzione(a)==false){
+    if (funzione (a)==false){
         cout << "errore" << endl;
     }else{
         cout << a << endl;
